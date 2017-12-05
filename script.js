b0e8c82ee4fde0fe74eb90d218c644eac730bb9c
kibiukibis <kibiukibis@gmail.com>
2017-12-05 12:45:44 +0200

    prekes

--- script.js
+++ script.js
@@ -1,51 +1,145 @@
-// surasti didziausia skaiciu masyve
-var numbers = [4, 7, 8, 2, 9, 5]
-var max = numbers[0];
-for (x = 1; x < numbers.length; x++) {
-    if (max < numbers[x]) {
-        max = numbers[x]
-    }
-}
-console.log(max)
+// // funkcijos aprasymas
 
-// vidiniai masyvai
-var prekes = [
-    ['pienas', 2.10, 1999],
-    ['vafliai', 3.15, 2000, 'neimsiu'],
-    ['tortas', 1.99, 2001]
-];
+// function getInfo(name = 'Benamis', age = 35) {
+//     console.log(`mano vardas ${name} ir amzius ${age}`);
+// }
 
-console.log(prekes[2][1]);
-for (x = 0; x < prekes.length; x++) {
-    for (y = 0; y < prekes[x].length; y++) {
-        console.log(prekes[x][y]);
-    }
-}
+// getInfo('Martynas', '32');
+// getInfo('Petras', '65');
+// getInfo();
 
+// // parametru skaiciavimas
+
+// function getShapeArea(a, b, type) {
+//     if (type === 'staciakampis') {
+//         var area = a * b;
+//         console.log('staciakampio plotas yra:', area);
+//         return area;
+//     } else if (type === 'trikampis') {
+//         var area = a * b / 2
+//         console.log('trikampio plotas yra:', area);
+//         return area;
+//     } else {
+//         console.error('Error, patikslinkite duomenis');
+//         return 0;
+//     }
+
+
+// }
+
+// // getShapeArea(8, 4, 'staciakampis');
+// // getShapeArea(8, 4, 'trikampis');
+// // getShapeArea(8, 4, 'bukakampis');
+
+// // getShapePerimeter()
+// function getShapePerimeter(a, b, type) {
+//     var perimeter;
+//     if (type === 'staciakampis') {
+//         perimeter = (a + b) * 2;
+//         console.log('staciakampio perimetras yra:', perimeter)
+//         return perimeter;
+//     } else if (type === 'trikampis') {
+//         perimeter = (Math.sqrt((a * a) + (b * b)) + a + b)
+//         console.log('trikampio perimetras yra:', perimeter)
+//         return perimeter;
+//     } else {
+//         console.error('Error, patikslinkite duomenis')
+//         return 0;
+//     }
+// }
+
+// // getShapePerimeter(3, 3, 'staciakampis')
+// // getShapePerimeter(3, 3, 'trikampis')
+// // getShapePerimeter(3, 3, 'bukakampis')
+
+// function getShapeInfo(a, b, type) {
+//     getShapeArea(a, b, type);
+//     getShapePerimeter(a, b, type);
+// }
+// getShapeInfo(8, 4, 'staciakampis');
+// getShapeInfo(8, 4, 'trikampis');
+// getShapeInfo(8, 4, 'bukakampis');
+
+
+
+// const shapes = [
+//     [52, 9, 'staciakampis'],
+//     [5, 91, 'trikampis'],
+//     [1, 12, 'staciakampis'],
+//     [5, 9, 'trikampis'],
+//     [5, 9, 'apskritimas'],
+//     [92, 9, 'trikampis'],
+// ];
+
+// // gauti pirmosisos fromos infromacija
+
+// var totalArea = 0;
+// var totalPerimeter = 0;
+// for (x = 0; x < shapes.length; x++) {
+//     getShapeInfo(shapes[x][0], shapes[x][1], shapes[x][2]);
+//     totalArea += getShapeArea(shapes[x][0], shapes[x][1], shapes[x][2]);
+//     totalPerimeter += getShapePerimeter(shapes[x][0], shapes[x][1], shapes[x][2]);
+// }
+// console.log(totalArea);
+// console.log(totalPerimeter);
 
 // ND
-var sk = [
-        [4, 8, 9],
-        [2, 6, 1],
-        [5, 7, 9, 5, 6, 7]
-    ]
-    // isvesti visus i konsole
-    //surasti nariu suma ir didziausia skaiciu
-    //Hard: 
+const prekes = [
+    ['suris', 2.23, 2019],
+    ['vynas', 11.23, 2021],
+    ['mesa', 4.99, 2016],
+    ['kefyras', 2.10, 2015]
+];
 
-var skaiciai = [6, 8, 34, 55, 78, 23, 34, 78, 97, 33, 2, 11];
+// #1 parasyti funkcija getProduct() kuri isveda paduotos prekes info isprintuoja pavadinima kaina ir galiojimo metus
+// #2 parasyti funkcija, kuri nustato ar preke yra pasenusi
+// #3 surasti bendra prekiu kaina
+// #4 parasyti funkcija kuri gauna argumenta bendra prekiu kaina ir mokamu pinigu suma. Ta funkcija paskaiciuoja graza
+// Hard: parasyti bendra funkcija kuri iskviecia vaikines funkcijas (#1,#2)
+console.warn(`Namu darbai`)
 
-
-var sorted = [];
-var totalLenght = skaiciai.length;
-
-for (y = 0; y < totalLenght; y++) {
-    var max = skaiciai[0];
-    for (x = 0; x < skaiciai.length; x++) {
-        if (skaiciai[x] > max) max = skaiciai[x]
+function getProduct() {
+    var x;
+    for (x = 0; x < prekes.length; x++) {
+        console.log(`${prekes[x][0]} ${prekes[x][1]} ${prekes[x][2]}`)
     }
-    sorted.unshift(max); //idedu didz. nari i nauja masyva
-    i = skaiciai.indexOf(max); //paskaiciuoju didiziausio nario indeksa
-    skaiciai.splice(i, 1); //istrinu surasta didz. nari is masyvo
-
 }
+
+function outDate() {
+    var x;
+    for (x = 0; x < prekes.length; x++) {
+        if (prekes[x][2] < 2017) {
+            console.error(`${prekes[x][0]} ${prekes[x][1]} ${prekes[x][2]} galiojimo laikas pasibaiges`)
+        }
+    }
+}
+
+var sum = 0;
+for (x = 0; x < prekes.length; x++) {
+    sum = sum + prekes[x][1];
+}
+sum = sum.toPrecision(4);
+var suma;
+
+function pay(paid, suma = sum) {
+    var change;
+    var paid;
+    console.log(`Sumoketa: ${paid}`)
+    console.log(`Bendra suma: ${suma}`)
+    if (sum > paid) {
+        console.warn(`Lesu nepakanka. Prekiu kaina: ${suma}`)
+    } else if (sum < paid) {
+        change = paid - suma;
+        console.log(`Graza: ${change}`)
+
+    }
+}
+
+
+function getProductStatus() {
+    getProduct();
+    outDate();
+}
+
+getProductStatus();
+pay(100);
